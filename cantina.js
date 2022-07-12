87c79b939f71543f05e4bf61f533024dbbcb3977
Toddynn <viniciusgabriez07@gmail.com>
2022-07-12 13:10:58 -0300

    s

--- cantina.js
+++ cantina.js
@@ -1,16 +1,16 @@
-var readline = require('readline');
-var resp = "";
-
-var leitor = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+let quant;
 const bisc = 2.5;
 const trento = 2;
 const bala = 0.5;
 const bombom = 1.5;
 const pacoca = 1;
+
+var readline = require('readline');
+var variavel = "";
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
     console.log("_________________________________________________________________");
     console.log("Bem vindo a cantina, informe o que deseja de acordo com a tabela: ");
@@ -21,38 +21,112 @@ const pacoca = 1;
     console.log("tecle 5 para Paçoca: 3 por 1,00");
     console.log("tecle 0 para finalizar a compra");
 
-
     leitor.question("Qual produto deseja? \n", function(answer){
-    let variavel = parseInt(answer);
-    let valor = 0;
-    let bla = variavel;
-    
-    while(bla != 0){
-        if(variavel != 0){
-            switch(variavel){
+        variavel = parseInt(answer);
+        leitor.close();
+
+    function top(){
+        switch(variavel){
             case 0: 
-                return;
-            case 1: 
-                valor = valor + bisc;
+                console.log("Você saiu!");
+                break;
+            case 1:
+                var leitorB = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                leitorB.question("Quantos Biscoitos vai querer?: \n", function(resp){
+                    quant = parseInt(resp);
+                    if(quant > 0){
+                        let res = quant * bisc;
+                        console.log("O valor da compra fica: R$" + res);
+                    }
+                    else{
+                        console.log("quantidade inválida");
+                    }
+                leitorB.close();
+                });
                 break;
             case 2: 
-                valor = valor + trento;
+                var leitorT = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                leitorT.question("Quantos Trentos vai querer?: \n", function(resp){
+                    quant = parseInt(resp);
+                    if(quant > 0){
+                        let res = quant * trento;
+                        console.log("O valor da compra fica: R$" + res);
+                    }
+                    else{
+                        console.log("quantidade inválida");
+                    }
+                leitorT.close();
+                });
                 break;
             case 3:
-                valor = valor + bala;
+                var leitorBala = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                leitorBala.question("Quantas Balas vai querer?: \n", function(resp){
+                    quant = parseInt(resp);
+                    if(quant > 0){
+                        let res = quant * (bala / 3);
+                        console.log("O valor da compra fica: R$" + res);
+                    }
+                    else{
+                        console.log("quantidade inválida");
+                    }
+                leitorBala.close();
+                });
                 break;
             case 4:
-                valor = valor + bombom;
+                var leitorS = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                leitorS.question("Quantos Sonhos de Valsa vai querer?: \n", function(resp){
+                    quant = parseInt(resp);
+                    if(quant > 0){
+                        let res = quant * bombom;
+                        console.log("O valor da compra fica: R$" + res);
+                    }
+                    else{
+                        console.log("quantidade inválida");
+                    }
+                leitorS.close();
+                });
                 break;
             case 5:
-                valor = valor + pacoca;
+                var leitorP = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                leitorP.question("Quantas Paçocas vai querer?: \n", function(resp){
+                    quant = parseInt(resp);
+                    if(quant > 0){
+                        let res = quant * (pacoca / 3);
+                        console.log("O valor da compra fica: R$" + res);
+                    }
+                    else{
+                        console.log("quantidade inválida");
+                    }
+                leitorP.close();
+                });
                 break;
             default:
                 console.log("valor inválido");    
-            }
         }
-
-        console.log("O valor de venda será de: R$" + valor);
-        leitor.close();
-    } 
+    }
+    while(variavel != 0){
+        return top();
+    }
 });
+
+
--- temperatura.js
+++ temperatura.js
@@ -23,48 +23,48 @@ var leitor = readline.createInterface({
             case 0: 
                 ;
             case 1:
-                var leito = readline.createInterface({
+                var leitorC = readline.createInterface({
                     input: process.stdin,
                     output: process.stdout
                 });
 
-                leito.question("Qual a temperatura Celsius?: \n", function(resp){
+                leitorC.question("Qual a temperatura Celsius?: \n", function(resp){
                     valor = parseInt(resp);
                     let res = (1.8*valor + 32);
                     let res2 = valor + 273.15;
                 console.log("A temperatura em Farenheit é: " + res + "ºF");
                 console.log("A temperatura em Kelvin é: " + res2 + "K");
-                leito.close();
+                leitorC.close();
                 });
                 break;
             case 2: 
-                var leit = readline.createInterface({
+                var leitorK = readline.createInterface({
                     input: process.stdin,
                     output: process.stdout
                 });
 
-                leit.question("Qual a temperatura Kelvin?: \n", function(resp){
+                leitorK.question("Qual a temperatura Kelvin?: \n", function(resp){
                     valor = parseInt(resp);
                     let res = valor - 273.15;
                     let res2 = ((valor - 273.15) * 9/5) + 32;
                 console.log("A temperatura em Celsius é: " + res + "ºC");
                 console.log("A temperatura em Farenheité: " + res2 + "ºF");
-                leit.close();
+                leitorK.close();
                 });
                 break;
             case 3:
-                var lei = readline.createInterface({
+                var leitorF = readline.createInterface({
                     input: process.stdin,
                     output: process.stdout
                 });
 
-                lei.question("Qual a temperatura Farenheit?: \n", function(resp){
+                leitorF.question("Qual a temperatura Farenheit?: \n", function(resp){
                     valor = parseInt(resp);
                     let res = ( (valor-32)*5/9 );
                     let res2 = ((valor - 32) * 5/9) + 273.15;
                 console.log("A temperatura em Celsius é: " + res + "ºC");
                 console.log("A temperatura em Kelvin é: " + res2 + "ºK")
-                lei.close();
+                leitorF.close();
                 });
                 break;
             default:
